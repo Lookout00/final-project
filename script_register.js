8e912be37316f117578d0d2b1f3ad58865d173cb
Adam <duckhook00@gmail.com>
2023-03-28 23:14:57 -0500

    formatted files with prettier

--- script_register.js
+++ script_register.js
@@ -1,10 +1,9 @@
 const signupBtn = document.querySelector("label.signup");
 const signupLink = document.querySelector("form .signup-link a");
-signupBtn.onclick = (()=>{
-});
-signupLink.onclick = (()=>{
-signupBtn.click();
-return false;
-});
+signupBtn.onclick = () => {};
+signupLink.onclick = () => {
+  signupBtn.click();
+  return false;
+};
 
 window.location.href = form.getAttribute("action");
